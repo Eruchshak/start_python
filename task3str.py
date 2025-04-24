7dfd766bb5445f8bf4429553570dfbd9716b0c21
Eruchshak <erusaksofa@gmail.com>
2025-04-24 12:10:00 +0300

    Add files via upload

---
+++ task3str.py
@@ -0,0 +1,43 @@
+# задача 1
+# vol = 1.44 * 1024 * 1024
+# page = 100
+# line = 50
+# simv = 25
+# byte = 4
+# volume_book = page * line * simv * byte
+# num_book = vol // volume_book
+# print(num_book)
+
+# задача 2
+# pi = 3.1415
+# r = 5
+# l = 5
+# s_circle = pi * r ** 2
+# l_circle = 2 * pi * r
+# p_square = 4 * l
+# s_square = l ** 2
+# print(round(s_circle ,2))
+# print(round(l_circle ,2))
+# print(round(p_square ,2))
+# print(round(s_square ,2))
+
+# задача 3
+# str = (20 * '0') + (50 * '1') + (30 * '2')
+# print(str)
+
+# задача 4
+# speed = 4096 / 1024
+# time = 120 * 60
+# coast = 0.125
+# free = 500
+# file = time * speed
+# money = (file - free) * coast
+# print(file)
+# print(money)
+
+# задача 5
+# lenght = 90
+# width = 50
+# perimetr = lenght + width + lenght + width
+# print(perimetr)
+
