baa0c8d2b82257a7abb5cdfbf43cb7c2faa0d729
Eruchshak <erusaksofa@gmail.com>
2025-06-09 12:23:26 +0300

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,37 @@
+# for value in collection # начало блока кода с телом цикла
+#     ...
+#     # rjytw ,kjrf rjlf c ntkjv wbrkf
+#     # код, который будет выполняться после цикл
+# for i in range (3)
+#     print (i+1, "шаг цикла") # будет выполнено 3 раза
+# print ("первый цикл закончился")
+# a = {1: 'x', 2: 'y', 3: 'z'}
+# for index value in a.items()
+#     print(f'"позиция: {index} -> значение: {value }"')
+# range(10) # генерация последовательности чисел от 0 до 9
+# range(1, 11) # генерация последовательности чисел от 0 до 10
+# range(1, 11, 2) # генерация последовательности чисел от 0 до 10 с шагом 2
+# range(1, 11, -1) # генерация последовательности чисел от 0 до 10 с шагом -1
+# for tuple_ in enumerate (["a", "б", "в"]):
+#     print(tuple_)
+# for pos, value in enumarete ("абв", start=1): # start можно указать любым чслом
+#     print("позиция:", pos, "->", "значение:", value)
+#
+# numbers = (10, 20, 30, 40, 50) # объявление списка чисел
+# total = 0 # todo заменить использование индексов на перебор значений списка
+# for i in range(len(numbers)):
+#      total +=[i]
+# print ("сумма чисел:", total) # сумма чисел 150
+# count = 0
+# while count < 5:
+#     print("значения count:", count)
+#     count += 1
+# sum_ = 0
+# input_number = int(input("введите число:"))
+# while input_number != 0:
+#     sum_ += input_number
+#     input_number = int(input("введите число:"))
+#     print("ответ:", sum_)
+
+a, s, p = 1, 150, 200
+while
